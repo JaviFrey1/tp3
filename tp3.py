3c1c2f3860d150dda5897c88bcb46f97810ebb91
Javi <81191050+JaviFrey1@users.noreply.github.com>
2025-04-27 22:20:31 -0300

    Create tp3.py
    ej1

---
+++ tp3.py
@@ -0,0 +1,9 @@
+TP3: Estructuras condicionales
+
+
+# Ej 1
+edad = int(input("Por favor, ingrese su edad: "))
+if edad > 18:
+    print("Es mayor de edad.")
+else:
+    print("No es mayor de edad.")
